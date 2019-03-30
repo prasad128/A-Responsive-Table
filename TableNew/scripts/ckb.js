b688c1a04fa10925edd2d6effd4036bbb3c9c687
prasad128 <48686696+prasad128@users.noreply.github.com>
2019-03-30 15:03:59 +0530

    Add files via upload

---
+++ TableNew/scripts/ckb.js
@@ -0,0 +1,45 @@
+/*let checkbox1 = document.querySelector("#ckb2")
+let checkbox2 = document.querySelector("#ckb3")
+if (checkbox1 == is.checked){
+    $("#name").text("hello");
+
+}*/
+
+/*$("#ckb2").click(function(){
+      $("#name").text("hello");
+});*/
+/*
+const checkbox2 = document.querySelectorAll(".ckb2");
+const name_chg = document.querySelector("#name")
+const team_access = document.querySelectorAll(".hoverH")
+checkbox2.addEventListener("click", update);
+
+function update(){
+    if checkbox2.checked == true{
+        name_chg.textContent = "hello world";
+    }
+    name_chg.textContent = "hello world";
+    
+}
+*/
+/*
+var NAME = document.querySelector("#name");
+
+NAME.style.color = "red";
+
+function getColor(){
+    letters = "0123456789ABCDEF";
+    color = "#";
+    for(i=0; i<6; i++){
+        randomC = letters[Math.floor(Math.random()*16)];
+        color += randomC;
+    }
+    return color;
+}
+function changeColor(){
+    var getC = getColor();
+    NAME.style.color = getC;
+}
+
+setInterval("changeColor()",500);
+*/
